151c258eff8dfc004f4892c3de5259683ef4c3fc
Lorraine-Olegario <lorraineolegario16@gmail.com>
2025-09-14 20:42:01 -0300

    update

--- src/test/java/com/fiap/techchallenge/external/api/HealthCheckControllerTest.java
+++ src/test/java/com/fiap/techchallenge/external/api/HealthCheckControllerTest.java
@@ -7,10 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
 /**
  * Testes para o controller de verificação de saúde da aplicação
  * <p>
