c4abe4efe806d80bb1ef04c299e038cc097b8960
Lorraine-Olegario <lorraineolegario16@gmail.com>
2025-10-03 20:11:20 -0300

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -40,12 +40,15 @@ data "terraform_remote_state" "core" {
 #   }
 # }
 
+
 data "aws_eks_cluster" "cluster" {
-  name = data.terraform_remote_state.core.outputs.eks_cluster_name
+  #name = data.terraform_remote_state.core.outputs.eks_cluster_name
+  name = "tech-challenge-eks"
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = data.terraform_remote_state.core.outputs.eks_cluster_name
+  #name = data.terraform_remote_state.core.outputs.eks_cluster_name
+  name = "tech-challenge-eks"
 }
 
 provider "kubernetes" {
